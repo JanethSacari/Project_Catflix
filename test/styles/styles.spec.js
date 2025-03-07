c085c53715ff9cbaaaccabdf2fbdc5054556e973
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2025-03-06 21:17:37 -0300

    Project Catflix - Path changed

--- test/styles/styles.spec.js
+++ test/styles/styles.spec.js
@@ -3,12 +3,12 @@ const puppeteer = require('puppeteer');
 test('O background da página deve ser preto', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('file:///C:/projeto/index.html');  // Caminho do arquivo
+    await page.goto('file:///C:/Users/janem/Projects/Coding/SpecialProjects/Project_Catflix/styles/main.css');
 
     const backgroundColor = await page.evaluate(() => {
         return window.getComputedStyle(document.body).backgroundColor;
     });
 
-    expect(backgroundColor).toBe('rgb(20, 20, 20)'); // Cor convertida de #141414
+    expect(backgroundColor).toBe('rgb(20, 20, 20)');
     await browser.close();
 });
